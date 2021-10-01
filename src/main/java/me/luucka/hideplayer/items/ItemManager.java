d9b823281ef41465cd896619fd97e97da3fa61e9
Luca Dalu <luca.dalu@outlook.it>
2021-10-01 22:55:30 +0200

    1.4 - Custom heads from minecraft-heads.com

--- src/main/java/me/luucka/hideplayer/items/ItemManager.java
+++ src/main/java/me/luucka/hideplayer/items/ItemManager.java
@@ -1,17 +1,18 @@
 package me.luucka.hideplayer.items;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import me.luucka.hideplayer.HidePlayer;
+import me.luucka.lcore.item.ItemBuilder;
 import me.luucka.lcore.utils.ColorTranslate;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.libs.org.apache.commons.codec.binary.Base64;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.persistence.PersistentDataType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.UUID;
 
 public class ItemManager {
 
@@ -27,29 +28,52 @@ public class ItemManager {
 
         String sMaterial = HidePlayer.getPlugin().getConfig().getString("item." + type + ".material");
 
-        ItemStack item = new ItemStack(Material.matchMaterial(sMaterial.equalsIgnoreCase("CUSTOM_HEAD") ? "PLAYER_HEAD" : sMaterial));
+        ItemBuilder itemBuilder;
 
         if (sMaterial.equalsIgnoreCase("CUSTOM_HEAD")) {
-            SkullMeta sMeta = (SkullMeta) item.getItemMeta();
-            if (sMeta == null) return null;
-            sMeta.setOwningPlayer(player);
-            item.setItemMeta(sMeta);
+            String headValue = HidePlayer.getPlugin().getConfig().getString("item." + type + ".custom-head-value");
+            if (headValue.isEmpty()) {
+                itemBuilder = new ItemBuilder(Material.PLAYER_HEAD);
+                itemBuilder.setOwningPlayer(player);
+            } else {
+                itemBuilder = new ItemBuilder(getCustomHead(headValue));
+            }
+        } else {
+            itemBuilder = new ItemBuilder(Material.matchMaterial(sMaterial));
         }
 
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) return null;
+        itemBuilder.setDisplayName(ColorTranslate.translate(HidePlayer.getPlugin().getConfig().getString("item." + type + ".name")));
+        itemBuilder.setLore((ColorTranslate.translate(HidePlayer.getPlugin().getConfig().getString("item." + type + ".lore"))));
+        itemBuilder.setPersistentDataContainer(HidePlayer.getPlugin(), "status", type.toUpperCase());
 
-        meta.setDisplayName(ColorTranslate.translate(HidePlayer.getPlugin().getConfig().getString("item." + type + ".name")));
-        List<String> lore = new ArrayList<>();
-        lore.add(ColorTranslate.translate(HidePlayer.getPlugin().getConfig().getString("item." + type + ".lore")));
-        meta.setLore(lore);
+        return itemBuilder.toItemStack();
+    }
 
-        NamespacedKey key = new NamespacedKey(HidePlayer.getPlugin(), "status");
-        meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, type.toUpperCase());
+    public static ItemStack getCustomHead(String url) {
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
+        if (url == null || url.isEmpty())
+            return skull;
 
-        item.setItemMeta(meta);
+        url = "http://textures.minecraft.net/texture/" + url;
 
-        return item;
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
+        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+        Field profileField = null;
+        try {
+            profileField = skullMeta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+        profileField.setAccessible(true);
+        try {
+            profileField.set(skullMeta, profile);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        skull.setItemMeta(skullMeta);
+        return skull;
     }
 
 }
